087e87ce23951910115e03515b50b9128fc89e70
David Tewkesbury <tewkesburyd@hotmail.com>
2022-12-19 15:08:55 -0700

    commit tree f0ef17df1f73d6724dff90b8db0493adba205209

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,10 @@
 class Character < ActiveRecord::Base
-  
+    belongs_to :actor
+    belongs_to :show
+
+    def say_that_thing_you_say
+        name = self.name
+        saying = self.catchphrase
+        "#{name} always says: #{saying}"
+    end
 end
